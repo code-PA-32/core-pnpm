d144b5960109dcd97abd5e80e35941b7ba012307
code-PA-32 <vernichenko.aleksandr@gmail.com>
2023-11-01 15:09:34 +0200

    feat: add graphql-yoga

--- be/app/src/index.ts
+++ be/app/src/index.ts
@@ -1,3 +1,4 @@
+import type { FastifyReply, FastifyRequest } from "fastify"
 import Fastify from "fastify"
 import supertokens from "supertokens-node"
 import Session from "supertokens-node/recipe/session"
@@ -5,9 +6,46 @@ import Passwordless from "supertokens-node/recipe/passwordless"
 import formDataPlugin from "@fastify/formbody"
 import cors from "@fastify/cors"
 import { plugin, errorHandler } from "supertokens-node/framework/fastify"
+import { createSchema, createYoga } from "graphql-yoga"
 
 const fastify = Fastify({
   logger: true,
+})
+
+const yoga = createYoga<{
+  req: FastifyRequest
+  reply: FastifyReply
+}>({
+  // Integrate Fastify logger
+  logging: {
+    debug: (...args) => args.forEach((arg) => fastify.log.debug(arg)),
+    info: (...args) => args.forEach((arg) => fastify.log.info(arg)),
+    warn: (...args) => args.forEach((arg) => fastify.log.warn(arg)),
+    error: (...args) => args.forEach((arg) => fastify.log.error(arg)),
+  },
+  schema: createSchema({
+    typeDefs: /* GraphQL */ `
+      scalar File
+
+      type Query {
+        hello: String
+        isFastify: Boolean
+      }
+      type Mutation {
+        hello: String
+        getFileName(file: File!): String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "world",
+      },
+      Mutation: {
+        hello: () => "world",
+        getFileName: (root, { file }: { file: File }) => file.name,
+      },
+    },
+  }),
 })
 
 supertokens.init({
@@ -40,6 +78,28 @@ void fastify.register(cors, {
 void fastify.register(formDataPlugin)
 void fastify.register(plugin)
 fastify.setErrorHandler(errorHandler())
+
+fastify.route({
+  // Bind to the Yoga's endpoint to avoid rendering on any path
+  url: yoga.graphqlEndpoint,
+  method: ["GET", "POST", "OPTIONS"],
+  handler: async (req, reply) => {
+    // Second parameter adds Fastify's `req` and `reply` to the GraphQL Context
+    const response = await yoga.handleNodeRequest(req, {
+      req,
+      reply,
+    })
+    response.headers.forEach((value, key) => {
+      void reply.header(key, value)
+    })
+
+    void reply.status(response.status)
+
+    void reply.send(response.body)
+
+    return reply
+  },
+})
 fastify.get("/", function get(request, reply) {
   void reply.send({ hello: "world" })
 })
